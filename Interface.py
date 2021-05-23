d837e45227906708ebab4567f981a32732f28a24
oF <sky0932039612@gmail.com>
2021-05-24 02:39:48 +0800

    完成大致架構

---
+++ Interface.py
@@ -0,0 +1,3 @@
+import manga
+a,b=map(str,input().split())
+manga.num(a,b)
--- manga.py
+++ manga.py
@@ -5,27 +5,22 @@ import os
 import time
 import random
 from bs4 import BeautifulSoup as bs
-
-
-def num(num):
+def num(num,address):
+    os.chdir(address)
     class nhview:
         def __init__(self):
             self.name = ""
             self.origin = ""
 
-        def random(self):
+        def randombook(self):
             self.name = str(random.randint(1, 400000))
-
-        def sao(self):
-            self.name = str(345545)
 
         def checklink(self):
             headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
             }
-    # 將輸入的號碼轉換成網址
-            self.origin = "https://nhentai.net/g/" + self.name + "/"
-            resp = requests.get(self.origin, headers=headers)
+            self.origin = "https://nhentai.net/g/" + self.name + "/"      #轉換成網址
+            resp = requests.get(self.origin, headers = headers)
             if resp.status_code == 200:
                 print("號碼：" + self.name)
                 print("連線成功")
@@ -35,32 +30,20 @@ def num(num):
                 print("連線代碼：" + str(resp.status_code))
                 print("連線錯誤，請重新輸入")
                 return False
-    # 輸入號碼
 
         def setlink(self):
-            while True:
-                self.name = num
-                if len(self.name) <= 6:
-                    break
-                else:
-                    print("輸入錯誤")
-
-            if self.name == "?":
-                self.random()
-            if self.name == "c8763":
-                self.sao()
+            self.name = num
 
         def setsail(self):
             headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
             }
-            for i in range(1, 2):
+            for i in range(1,10000):
                 url = self.origin + str(i)
-    # 新增資料夾
-                nowpath = os.path.abspath('./demo')
+                nowpath = os.path.abspath('./' + self.name)        #獲取當前路徑
 
                 try:
-                    req = ur.Request(url=url, headers=headers)
+                    req = ur.Request(url=url,headers=headers)
                     result = ur.urlopen(req).read().decode('utf-8')
                 except urllib.error.URLError as e:
                     print(e.reason)
@@ -68,13 +51,14 @@ def num(num):
                     break
                 else:
                     if not os.path.isdir(nowpath):
-                        print("新增資料夾："+self.name)
+                        print("新增資料夾。")
                         os.mkdir(nowpath)
                     html = bs(result)
                     img = html.select('section#image-container img')[0]
                     imgurl = img.get('src')
-                    ur.urlretrieve(imgurl, os.path.join(
-                        nowpath,'MangaIMG.jpg'), reporthook=None, data=None)
+                    ur.urlretrieve(imgurl, os.path.join(nowpath, str(i) + '.jpg'), reporthook=None, data=None)
+                    print("已增加第" + str(i) + "頁。")
+                    time.sleep(0.3)
 
         def start(self):
             self.setlink()
@@ -82,9 +66,9 @@ def num(num):
             while True:
                 if self.checklink() == True:
                     break
-                else:
+            else:
                     self.setlink()
-
+        
             self.setsail()
 
     nh = nhview()
