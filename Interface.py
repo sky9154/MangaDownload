17296adc35008ccd4f64b9e0971b48ac7999771b
oF <sky0932039612@gmail.com>
2021-05-25 02:53:31 +0800

    美化視窗

--- Interface.py
+++ Interface.py
@@ -1,9 +1,12 @@
-import manga
-from tkinter import filedialog
-import os
-import tkinter as tk
-num=0
-def files():
+#-----------------------模組-----------------------
+
+import manga                              #漫畫下載模組
+from tkinter import filedialog     #檔案選擇視窗
+import tkinter as tk                    #視窗模組
+
+#-----------------------副程式---------------------
+
+def files():    #檔案選擇視窗
     address=tk.Tk()
     address.withdraw()
     curdir = os.getcwd()
@@ -11,13 +14,27 @@ def files():
     if len(tempdir) > 0:
         print ("目前檔案位置: %s" % tempdir)
     return tempdir
-def output():
+
+def output():   #漫畫下載
     manga.num(e.get(),files())
+
+#-----------------------視窗參數-------------------
+
 window = tk.Tk()
-window.title('漫畫下載器')
-window.configure(background='black')
-height_label = tk.Label(window, text='漫畫號碼：').pack(side=tk.LEFT)
-e = tk.Entry(window)
+window.title('漫畫下載器')  #標題
+window.configure(background='black') #背景顏色
+window.resizable(0,0)   #固定大小
+window.geometry("+500+100")    #開啟座標
+
+#-----------------------視窗元件-------------------
+
+label = tk.Label(window, text='漫畫編號：')
+label.configure(font=("Arial"))
+e = tk.Entry(window,font="Arial",bd=1)
+label.pack(side=tk.LEFT)
 e.pack(side=tk.LEFT)
-tk.Button(window, text='確定',command=output).pack(side=tk.LEFT)
+tk.Button(window,text='確定',command=output).pack(side=tk.LEFT)
+
+#-----------------------啟動視窗-------------------
+
 window.mainloop()
---
+++ launcher.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter import font
+import os, time
+
+win = Tk()
+win.minsize(200, 40)
+win.maxsize(200, 40)
+win.attributes('-topmost', 1)
+
+font = font.Font(family="DisposableDroid BB", size=30)
+
+def start_py():
+	os.system("D:\雜物\程式設計\Python\工具\漫畫下載器\Interface.py")
+	return
+
+btn = Button(win, text='START', command=start_py)
+btn.config(width=100, font=font, relief=FLAT, activebackground='#f02468', activeforeground='white', bg='#3df2a4', fg='#323232')
+btn.pack()
+
+win.mainloop()
