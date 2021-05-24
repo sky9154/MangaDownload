d49fdca76493c1278acedcfa270e77173a763172
oF <sky0932039612@gmail.com>
2021-05-24 09:53:45 +0800

    新增選擇檔案視窗

--- Interface.py
+++ Interface.py
@@ -1,3 +1,15 @@
 import manga
-a,b=map(str,input().split())
-manga.num(a,b)
+import tkinter
+from tkinter import filedialog
+import os
+root = tkinter.Tk()
+root.withdraw()
+def files():
+    currdir = os.getcwd()
+    tempdir = filedialog.askdirectory(parent=root, initialdir=currdir, title='選擇資料夾儲存位置')
+    if len(tempdir) > 0:
+        print ("目前檔案位置: %s" % tempdir)
+    return tempdir
+files= files()
+num=input()
+manga.num(num,files)
